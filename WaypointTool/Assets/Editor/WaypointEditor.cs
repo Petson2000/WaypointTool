9566c80402cd7c751b169ba6c0fa1da01c0bbe71
petson123 <erik.p2000@hotmail.com>
2020-04-20 17:04:28 +0200

    -Added Handles that can be dragged around by designer -Added Undo and Redo changes to waypoints - Half way finished on character using waypoints

---
+++ WaypointTool/Assets/Editor/WaypointEditor.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(Character))]
+public class WaypointEditor : Editor
+{
+    private Character character;
+
+    private SerializedObject so;
+    private SerializedProperty wayPoints;
+    
+
+    private void OnEnable()
+    {
+        so = serializedObject;
+
+        character = (Character) target;
+        
+        wayPoints = so.FindProperty("wayPoints");
+
+        Selection.selectionChanged += Repaint;
+    }
+
+    //Draw in inspector
+    public override void OnInspectorGUI()
+    {
+        so.Update();
+        
+        EditorGUILayout.PropertyField(wayPoints);
+        
+        if (so.ApplyModifiedProperties())
+        {
+            Repaint();
+        }
+    }
+
+    //Draw in the scene
+    private void OnSceneGUI()
+    {
+        Handles.color = Color.green;
+
+        for (int i = 0; i < character.wayPoints.Length; i++)
+        {
+           character.wayPoints[i] = Handles.PositionHandle(character.wayPoints[i], Quaternion.identity);
+        }
+
+        for (int i = 0; i < character.wayPoints.Length; i++)
+        {
+            if (character.wayPoints[i] != null)
+            {
+                Handles.DrawDottedLine(character.wayPoints[i], character.wayPoints[(int)Mathf.Repeat(i + 1, character.wayPoints.Length)], 15f);
+            }
+        }
+    }
+
+    private void GetWaypoints()
+    {
+    }
+}
---
+++ WaypointTool/Assets/Scripts/Character.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Character : MonoBehaviour
+{
+    public Vector3[] wayPoints;
+    public int currentWaypoint = 0;
+    public Vector3 targetPos;
+
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        targetPos = wayPoints[currentWaypoint];
+
+        if (transform.position != targetPos)
+        {
+            agent.SetDestination(targetPos);
+            //transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        }
+
+        else
+        {
+            if (wayPoints[currentWaypoint] != wayPoints[wayPoints.Length - 1])
+            {
+                currentWaypoint++;
+            }
+        }
+    }
+
+    public Vector3[] GetWaypoints()
+    {
+        return wayPoints;
+    }
+}
